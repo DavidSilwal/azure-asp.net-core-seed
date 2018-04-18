46679c1dc0222c98b1c8435d52bec5b44ab15182
rameshksh <ramesh.sharma12@gmail.com>
2018-04-18 17:48:24 +0530

    add .net core project.

---
+++ PhotoGallery/PhotoGallery/Startup.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using PhotoGallery.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using PhotoGallery.Infrastructure.Repositories;
+using PhotoGallery.Infrastructure.Services;
+using PhotoGallery.Infrastructure.Mappings;
+using System.Security.Claims;
+using System.IO;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Newtonsoft.Json.Serialization;
+using System;
+
+namespace PhotoGallery
+{
+    public class Startup
+    {
+        private static string _applicationPath = string.Empty;
+        private static string _contentRootPath = string.Empty;
+        public Startup(IHostingEnvironment env)
+        {
+            _applicationPath = env.WebRootPath;
+            _contentRootPath = env.ContentRootPath;
+            // Setup configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(_contentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string sqlConnectionString = Configuration["ConnectionStrings:DefaultConnection"];
+
+            bool useInMemoryProvider = bool.Parse(Configuration["Data:PhotoGalleryConnection:InMemoryProvider"]);
+
+            // Use SQL Database if in Azure, otherwise, use SQLite
+            if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production")
+            {
+                sqlConnectionString = Configuration["ConnectionStrings:ProductionConnection"];
+            }
+
+            services.AddDbContext<PhotoGalleryContext>(options => {
+                switch (useInMemoryProvider)
+                {
+                    case true:
+                        options.UseInMemoryDatabase("PhotoGalleryDB");
+                        break;
+                    default:
+                        options.UseSqlServer(sqlConnectionString);
+                    break;
+                }
+            });
+
+            services.BuildServiceProvider().GetService<PhotoGalleryContext>().Database.Migrate();
+
+            // Repositories
+            services.AddScoped<IPhotoRepository, PhotoRepository>();
+            services.AddScoped<IAlbumRepository, AlbumRepository>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddScoped<IUserRoleRepository, UserRoleRepository>();
+            services.AddScoped<IRoleRepository, RoleRepository>();
+            services.AddScoped<ILoggingRepository, LoggingRepository>();
+
+            // Services
+            services.AddScoped<IMembershipService, MembershipService>();
+            services.AddScoped<IEncryptionService, EncryptionService>();
+
+            services.AddAuthentication();
+
+            // Polices
+            services.AddAuthorization(options =>
+            {
+                // inline policies
+                options.AddPolicy("AdminOnly", policy =>
+                {
+                    policy.RequireClaim(ClaimTypes.Role, "Admin");
+                });
+
+            });
+
+            // Add MVC services to the services container.
+            services.AddMvc()
+            .AddJsonOptions(opt =>
+            {
+                var resolver = opt.SerializerSettings.ContractResolver;
+                if (resolver != null)
+                {
+                    var res = resolver as DefaultContractResolver;
+                    res.NamingStrategy = null;
+                }
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            // this will serve up wwwroot
+            app.UseStaticFiles();
+
+            AutoMapperConfiguration.Configure();
+
+            // Custom authentication middleware
+            //app.UseMiddleware<AuthMiddleware>();
+
+            // Add MVC to the request pipeline.
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                //routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
+            });
+
+            DbInitializer.Initialize(app.ApplicationServices, _applicationPath);
+        }
+    }
+}
